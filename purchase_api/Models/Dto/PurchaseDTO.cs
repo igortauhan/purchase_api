8746b68130a909904a7fa79c1a168b5eb1f1849c
Igor Tauhan <igortauhan.ds31@gmail.com>
2021-10-13 13:45:15 -0300

    Added validations

--- purchase_api/Models/Dto/PurchaseDTO.cs
+++ purchase_api/Models/Dto/PurchaseDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,10 @@ namespace purchase_api.Models.Dto
     public class PurchaseDTO
     {
         public long Id { get; set; }
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
         public string Name { get; set; }
+        [Required]
         public double Value { get; set; }
         public DateTime BuyDate { get; set; }
 
